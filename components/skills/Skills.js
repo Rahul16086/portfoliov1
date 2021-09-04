8462dd0aa920b9e48d9ce1d5630c53472450d11d
Rahul <rahul16086@gmail.com>
2021-09-04 12:11:53 +0530

    Fixed margin in skill component and centered the text in mobileview-sidebar

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -4,31 +4,33 @@ import classes from "./Skills.module.css";
 const SkillsComponent = () => {
   return (
     <div className={classes.mainContainer}>
-      <div className={classes.consolidatedSkills}>
-        <div className={classes.bar}>
-          <div className={classes.skillName}>
-            <span>Front-end</span>
+      <div className={classes.firstHalf}>
+        <div className={classes.consolidatedSkills}>
+          <div className={classes.bar}>
+            <div className={classes.skillName}>
+              <span>Front-end</span>
+            </div>
+            <div className={classes.barProgressFE}>
+              <span></span>
+            </div>
           </div>
-          <div className={classes.barProgressFE}>
-            <span></span>
-          </div>
-        </div>
 
-        <div className={classes.bar}>
-          <div className={classes.skillName}>
-            <span>Back-end</span>
+          <div className={classes.bar}>
+            <div className={classes.skillName}>
+              <span>Back-end</span>
+            </div>
+            <div className={classes.barProgressBE}>
+              <span></span>
+            </div>
           </div>
-          <div className={classes.barProgressBE}>
-            <span></span>
-          </div>
-        </div>
 
-        <div className={classes.bar}>
-          <div className={classes.skillName}>
-            <span>React</span>
-          </div>
-          <div className={classes.barProgressRE}>
-            <span></span>
+          <div className={classes.bar}>
+            <div className={classes.skillName}>
+              <span>React</span>
+            </div>
+            <div className={classes.barProgressRE}>
+              <span></span>
+            </div>
           </div>
         </div>
       </div>

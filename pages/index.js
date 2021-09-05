e00e968fa6ee9ac21e2ec849bda01a28cba80954
Rahul <rahul16086@gmail.com>
2021-09-05 19:20:46 +0530

    Revert back to original, deleted document js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import HomePage from "../components/homePage/HomePage";
+import Head from "next/head";
 
 const Home = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Head>
+        <meta name={"description"} content={"A simple portfolio"} />
+        <meta name={"viewport"} content={"initial-scale=1"} />
+        <html lang={"en"} />
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <title>Rahul's Portfolio</title>
+      </Head>
+      <HomePage />
+    </>
+  );
 };
 
 export default Home;

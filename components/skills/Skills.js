30a36121cf749df90e1a657ae7fd8e0ebc70556e
Rahul <rahul16086@gmail.com>
2021-09-05 13:59:01 +0530

    Removed unnecessary data and added separate data file for Skills component

--- components/skills/Skills.js
+++ components/skills/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Skills.module.css";
+import { SkillsData } from "./SkillsData";
 
 const SkillsComponent = () => {
   return (
@@ -36,99 +37,16 @@ const SkillsComponent = () => {
       </div>
 
       <div className={classes.skillsContainer}>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Languages</h1>
-            <p>
-              Javascript <br />
-              Python <br />
-              Typescript
-              <br /> Java
-              <br /> C
-            </p>
+        {SkillsData.map((skill, key) => (
+          <div className={classes.skills} key={key}>
+            <div className={classes.individual}>
+              <h1>{skill.SkillTitle}</h1>
+              {skill.Skills.map((skillName, key) => (
+                <p key={key}>{skillName}</p>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Front-end</h1>
-            <p>
-              HTML <br />
-              CSS <br />
-              React
-              <br />
-              Bootstrap
-            </p>
-          </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Back-end</h1>
-            <p>
-              Firebase
-              <br /> NodeJS
-            </p>
-          </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Mobile</h1>
-            <p>Android Studio</p>
-          </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Database</h1>
-            <p>
-              SQL
-              <br />
-              Firestore
-              <br />
-              MongoDB
-            </p>
-          </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>Version Control</h1>
-            <p>
-              Git
-              <br />
-              Github
-            </p>
-          </div>
-        </div>
-        <div className={classes.skills}>
-          <div className={classes.individual}>
-            <h1>UI/UX</h1>
-            <p>
-              Figma
-              <br />
-              Adobe Photoshop
-            </p>
-          </div>
-        </div>
-        {/*</div>*/}
-        {/*<div>*/}
-        {/*  <h1>UI/UX</h1>*/}
-        {/*  <p>Figma</p>*/}
-        {/*</div>*/}
-        {/*  </p>*/}
-        {/*  <h1>Mobile</h1>*/}
-        {/*  <p>Android Studio</p>*/}
-        {/*</div>*/}
-        {/*<div className={classes.skills}>*/}
-        {/*  <h1>Database</h1>*/}
-        {/*  <p>*/}
-        {/*    SQL*/}
-        {/*    <br />*/}
-        {/*    Firestore*/}
-        {/*    <br />*/}
-        {/*    MongoDB*/}
-        {/*  </p>*/}
-        {/*  <h1>Version Control</h1>*/}
-        {/*  <p>Git Github</p>*/}
-        {/*</div>*/}
-        {/*</div>*/}
+        ))}
       </div>
     </div>
   );
---
+++ components/skills/SkillsData.js
@@ -0,0 +1,18 @@
+export const SkillsData = [
+  {
+    SkillTitle: "Languages",
+    Skills: ["Javascript", "Python", "Typescript", "Java", "C"],
+  },
+  {
+    SkillTitle: "Front-end",
+    Skills: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    SkillTitle: "Back-end",
+    Skills: ["Firebase", "NodeJS"],
+  },
+  { SkillTitle: "Mobile", Skills: ["Android Studio"] },
+  { SkillTitle: "Database", Skills: ["SQL", "Firestore", "MongoDB"] },
+  { SkillTitle: "Version Control", Skills: ["Git", "Github"] },
+  { SkillTitle: "UI/UX", Skills: ["Figma", "Adobe", "Photoshop"] },
+];

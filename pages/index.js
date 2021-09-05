05df98aa3aae43edc1fd13515c04026e32736f51
Rahul <rahul16086@gmail.com>
2021-09-05 16:39:28 +0530

    Added title and meta on Head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Head from "next/head";
 import HomePage from "../components/homePage/HomePage";
 
 const Home = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Head>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <title>Rahul's Portfolio</title>
+        <meta name={"description"} content={"A simple portfolio"} />
+      </Head>
+      <HomePage />
+    </>
+  );
 };
 
 export default Home;
